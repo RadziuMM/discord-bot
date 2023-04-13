9c83f066f1e43670e92103f4788fb44418b09de7
Radek <RMazurczak@protonmail.com>
2023-04-13 17:37:55 +0200

    feat: discord update fix

--- src/tasks/music/utils/joinRoom/index.ts
+++ src/tasks/music/utils/joinRoom/index.ts
@@ -48,6 +48,21 @@ export default async (
     }
   });
 
+  room.player.on('connectionCreate', (queue) => {
+    queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
+      const oldNetworking = Reflect.get(oldState, 'networking');
+      const newNetworking = Reflect.get(newState, 'networking');
+
+      const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+        const newUdp = Reflect.get(newNetworkState, 'udp');
+        clearInterval(newUdp?.keepAliveInterval);
+      }
+
+      oldNetworking?.off('stateChange', networkStateChangeHandler);
+      newNetworking?.on('stateChange', networkStateChangeHandler);
+    });
+  });
+
   room.player.on('error', async (error: any) => {
     logger(`A problem was encountered while playing a song. Error: ${error.message}`, LogType.ERROR);
     room.songs.shift();
